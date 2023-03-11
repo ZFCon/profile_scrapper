15874675f2f038c8b2d535f55795d0eb1425d35f
Ziad Ahmed <zaid.ahmed@quickcheck.ng>
2023-03-11 15:48:32 +0200

    .

--- runner/management/commands/_scraper.py
+++ runner/management/commands/_scraper.py
@@ -1,8 +1,5 @@
 import pandas as pd
 from scrapy import Selector
-
-from selenium import webdriver
-import undetected_chromedriver as uc
 from runner.models import Configuration
 
 
@@ -11,58 +8,56 @@ from runner.models import Configuration
 class PeopleFreeSearchCrawler():
     configuration = Configuration.get_solo()
 
-    def __init__(self, input_file='./searchpeoplefree.csv'):
+    def __init__(self, driver, input_file='./searchpeoplefree.csv'):
         self.input_file = input_file
         self.df = pd.read_csv(self.input_file)
-        self.chrome_options = webdriver.ChromeOptions()
-        self.chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+        self.driver = driver
     
     def start(self):
         self.configuration.refresh_from_db()
-        with uc.Chrome(version_main=109, options=self.chrome_options) as driver:
-            driver.maximize_window()
-            index = self.configuration.skip_traced
-            total_count = self.df.shape[0]
+            
+        index = self.configuration.skip_traced
+        total_count = self.df.shape[0]
 
-            self.configuration.total_count = total_count
+        self.configuration.total_count = total_count
+        self.configuration.save()
+
+        for index, row in self.df.iloc[index:,:].iterrows():
+            self.configuration.refresh_from_db()
+            # save progress.
+            self.configuration.skip_traced = index
             self.configuration.save()
+            
+            if not self.configuration.should_run:
+                break
 
-            for index, row in self.df.iloc[index:,:].iterrows():
-                self.configuration.refresh_from_db()
-                # save progress.
-                self.configuration.skip_traced = index
-                self.configuration.save()
+            url = row['Link']
+            print('Scraping... {}/{} === {}'.format(index, total_count, url))
+            
+            try:
+                self.driver.get(url)
+            except:
+                print('Fail to get!')
+                continue
+
+            selector = Selector(text=self.driver.page_source)
+            name = selector.css('ol.inline > li:nth-child(1) article span.d-block::text').get()
+            if not name:
+                name = selector.css('ol.inline > li:nth-child(1) article h2::text').get()
+            if not name:
+                continue
                 
-                if not self.configuration.should_run:
-                    break
+            phones = selector.xpath('//ol/li[1]//article[1]//ul//a[contains(@href, "phone-lookup")]//text()').extract()
+            for n, phone in enumerate(phones):
+                self.df.loc[index, 'Phone {}'.format(n+1)] = phone
 
-                url = row['Link']
-                print('Scraping... {}/{} === {}'.format(index, total_count, url))
-                
-                try:
-                    driver.get(url)
-                except:
-                    print('Fail to get!')
-                    continue
-
-                selector = Selector(text=driver.page_source)
-                name = selector.css('ol.inline > li:nth-child(1) article span.d-block::text').get()
-                if not name:
-                    name = selector.css('ol.inline > li:nth-child(1) article h2::text').get()
-                if not name:
-                    continue
-                    
-                phones = selector.xpath('//ol/li[1]//article[1]//ul//a[contains(@href, "phone-lookup")]//text()').extract()
-                for n, phone in enumerate(phones):
-                    self.df.loc[index, 'Phone {}'.format(n+1)] = phone
-
-                self.df.loc[index, 'Name'] = name.strip()
-                self.df.to_csv(self.input_file, index=False)
-                
-                if index + 1 == total_count:
-                    self.configuration.skip_traced = 0
-                    self.configuration.should_run = False
-                    self.configuration.save()   
+            self.df.loc[index, 'Name'] = name.strip()
+            self.df.to_csv(self.input_file, index=False)
+            
+            if index + 1 == total_count:
+                self.configuration.skip_traced = 0
+                self.configuration.should_run = False
+                self.configuration.save()   
 
 if __name__ == '__main__':
     scraper = PeopleFreeSearchCrawler()
--- runner/management/commands/runner.py
+++ runner/management/commands/runner.py
@@ -2,6 +2,8 @@ from django.core.management.base import BaseCommand
 from runner.models import Configuration
 import time
 from ._scraper import PeopleFreeSearchCrawler
+import undetected_chromedriver as uc
+from selenium import webdriver
 
 SLEEP_TIME = 1
 
@@ -10,12 +12,20 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         configuration = Configuration.get_solo()
+
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+        driver = uc.Chrome(version_main=109, options=chrome_options)
+        driver.maximize_window()
+        import pdb
+        pdb.set_trace()
+
         while True:
             configuration.refresh_from_db()
 
             try:
                 if configuration.should_run:
-                    scraper = PeopleFreeSearchCrawler(configuration.runner_file.path)
+                    scraper = PeopleFreeSearchCrawler(driver, configuration.runner_file.path)
 
                     scraper.start()
 
