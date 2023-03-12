870688b7bf7583d94dbfe55de673afb87265008b
Ziad Ahmed <zaid.ahmed@quickcheck.ng>
2023-03-12 17:17:15 +0200

    '

--- runner/management/commands/runner.py
+++ runner/management/commands/runner.py
@@ -27,7 +27,7 @@ class Command(BaseCommand):
 
                     scraper.start()
 
-                    self.configuration.refresh_from_db()
+                    configuration.refresh_from_db()
                     configuration.should_run = False
                     configuration.save()
             except:
